344b0659a3ca471088de1fd8446e403beace153c
hyeonggyu <hyeonggyu.Kang@hyperlounge.ai>
2023-06-25 16:59:25 +0900

    blazor EditForm 테스트

---
+++ BlazorApp/BlazorApp/Models/ExampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
+{
+    public class ExampleModel
+    {
+        [Required (ErrorMessage = "이름을 입력 해 주세요.")]
+        [StringLength(10, ErrorMessage ="10자 이내로 입력 해 주세요.")]
+        public string Name { get; set; }
+    }
+}
